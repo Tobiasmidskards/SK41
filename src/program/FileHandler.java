5741b56c0226e544113c1d23af9649fa2525dfa3
Frederik Søndergaard Jensen <FSj95@hotmail.dk>
2018-12-04 17:20:14 +0100

    Get missed payments

--- src/program/FileHandler.java
+++ src/program/FileHandler.java
@@ -7,6 +7,8 @@ public class FileHandler{
 
   private File file;
   private Scanner scanner;
+	private FileWriter fileWriter;
+	private PrintWriter printWriter;
 
   public FileHandler(){
 
@@ -32,5 +34,26 @@ public class FileHandler{
 
     return scanner;
   }
+
+
+	public void writeMissedPayment(String fileName, ArrayList<ArrayList<String>> allMembers){
+		try {
+			int totalMissing = 0;
+			FileWriter fileWriter = new FileWriter(fileName);
+    	PrintWriter printWriter = new PrintWriter(fileWriter);
+    	printWriter.print("FØLGENDE MEDLEMMER MANGLER AT BETALE: \n\n");
+			for (ArrayList<String> member : allMembers){
+
+					printWriter.printf("%s %s mangler at betale %skr.\n", member.get(7), member.get(8), member.get(4).substring(1));
+					totalMissing = totalMissing + Integer.parseInt(member.get(4));
+			}
+			printWriter.printf("\n DER MANGLER I ALT AT BLIVE BETALT: %d", Math.abs(totalMissing));
+    	printWriter.close();
+		} catch (IOException e) {
+
+		}
+	}
+
+
 
 }
